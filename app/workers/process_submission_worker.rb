59ab64e54c45f9e2c4608df35c194ee66dfebbd2
Emile <emile.swarts123@gmail.com>
2019-07-19 10:00:09 +0100

    Restore job functionality

--- app/workers/process_submission_worker.rb
+++ app/workers/process_submission_worker.rb
@@ -2,8 +2,7 @@ class ProcessSubmissionWorker
   include Sidekiq::Worker
 
   def perform(submission_id)
-    echo "Running"
-    # service = ProcessSubmissionService.new(submission_id: submission_id)
-    # service.perform
+    service = ProcessSubmissionService.new(submission_id: submission_id)
+    service.perform
   end
 end
