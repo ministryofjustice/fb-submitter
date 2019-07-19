20301cdd20342903cea053050f7d51532e14c717
Emile <emile.swarts123@gmail.com>
2019-07-19 09:43:16 +0100

    NOOP sidekiq worker logic
    Still seeing memory leaks

--- app/workers/process_submission_worker.rb
+++ app/workers/process_submission_worker.rb
@@ -2,7 +2,8 @@ class ProcessSubmissionWorker
   include Sidekiq::Worker
 
   def perform(submission_id)
-    service = ProcessSubmissionService.new(submission_id: submission_id)
-    service.perform
+    echo "Running"
+    # service = ProcessSubmissionService.new(submission_id: submission_id)
+    # service.perform
   end
 end
