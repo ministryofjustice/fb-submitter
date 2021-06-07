2025fda3b3cdd7a8f208c050dd8ba945d5afe757
Tomas D'Stefano <tomas_stefano@successoft.com>
2021-06-06 23:09:19 -0300

    Writing some fake code that won't download in paralell

--- app/jobs/v2/process_submission_job.rb
+++ app/jobs/v2/process_submission_job.rb
@@ -41,13 +41,66 @@ module V2
     end
 
     def download_attachments(attachments, encrypted_user_id_and_token, access_token)
-      DownloadService.new(
+      DownloadAttachments.new(
         attachments: attachments,
         token: encrypted_user_id_and_token,
         access_token: access_token,
         jwt_skew_override: nil,
         target_dir: nil
-      ).download_in_parallel
+      ).download
     end
+  end
+end
+
+class DownloadAttachments
+  attr_reader :attachments, :target_dir, :token, :access_token
+
+  def initialize(attachments:, target_dir: nil, token:, access_token:, jwt_skew_override:)
+    @attachments = attachments
+    @target_dir = target_dir
+    @token = token
+    @access_token = access_token
+    @jwt_skew_override = jwt_skew_override
+  end
+
+  def download
+    actual_dir = target_dir || Dir.mktmpdir
+
+    attachments.each do |attachment|
+      url = attachment.fetch('url')
+      filename = attachment.fetch('filename')
+      mimetype = attachment.fetch('mimetype')
+      tmp_path = file_path_for_download(url: url, target_dir: actual_dir)
+      request(url: url, file_path: tmp_path, headers: headers)
+      results << Attachment.new(url: url, path: tmp_path, filename: filename, mimetype: mimetype)
+    end
+
+    results
+  end
+
+  private
+
+  def request(url:, file_path:, headers:)
+    response = Faraday.get(url, {}, headers)
+
+    File.open(file_path, 'wb') do |f|
+      f.write(response.body)
+    end
+  end
+
+  attr_reader :jwt_skew_override
+
+  def headers
+    {
+      'x-encrypted-user-id-and-token' => token,
+      'x-access-token-v2' => access_token,
+      'x-jwt-skew-override' => jwt_skew_override
+    }.compact
+  end
+
+  def file_path_for_download(url:, target_dir: nil)
+    actual_dir = target_dir || Dir.mktmpdir
+    filename = File.basename(URI.parse(url).path)
+    File.join(actual_dir, filename)
   end
 end
