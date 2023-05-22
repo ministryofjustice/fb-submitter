e4031e65a851861c581e963d68d1bb068cef950f
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-05-22 15:36:40 +0100

    Catching exception if it cannot parse the mimetype

--- app/services/attachment_parser_service.rb
+++ app/services/attachment_parser_service.rb
@@ -9,11 +9,13 @@ class AttachmentParserService
       Rails.logger.info attachment.to_s
       Attachment.new(
         url: attachment.fetch(:url, nil),
-        mimetype: attachment.fetch(:mimetype) { MIME::Types.type_for(attachment[:filename]).first.content_type },
+        mimetype: attachment.fetch(:mimetype),
         filename: attachment.fetch(:filename),
         type: attachment.fetch(:type),
         path: nil
       )
+    rescue KeyError
+      Rails.logger.info 'Couldn\'t parse the attachment and will skip it'
     end
   end
 
