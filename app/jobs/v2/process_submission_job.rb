dd90a981fcff491254dba2b6cd4d7cb9af9d10c8
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-05-23 15:58:01 +0100

    Adding support for both v1 and v2 submission

--- app/jobs/v2/process_submission_job.rb
+++ app/jobs/v2/process_submission_job.rb
@@ -12,7 +12,7 @@ module V2
         when 'json'
           JsonWebhookService.new(
             webhook_attachment_fetcher: WebhookAttachmentService.new(
-              attachment_parser: AttachmentParserService.new(attachments: payload_service.attachments),
+              attachment_parser: AttachmentParserService.new(attachments: payload_service.attachments, v2submission: true),
               user_file_store_gateway: Adapters::UserFileStore.new(key: submission.encrypted_user_id_and_token)
             ),
             webhook_destination_adapter: Adapters::JweWebhookDestination.new(
